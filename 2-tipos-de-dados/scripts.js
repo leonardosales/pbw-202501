5146b39f0e6c808192c63238fbef225002558f7e
Leonardo Sales <leonardosales18@gmail.com>
2025-04-16 20:33:51 -0300

    Tipo de dado: Number

---
+++ 2-tipos-de-dados/scripts.js
@@ -0,0 +1,10 @@
+// Number
+console.log(typeof 13);
+console.log(typeof 1.8);
+console.log(typeof -5);
+console.log(typeof (2 + 2));
+console.log(typeof (2 * 4 - 3));
+console.log(10 % 2);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
