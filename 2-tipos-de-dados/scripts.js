2251f2a9e428dde10ce2408ce3ecc240e842b983
Leonardo Sales <leonardosales18@gmail.com>
2025-04-16 21:18:18 -0300

    Tipo de dado: String

--- 2-tipos-de-dados/scripts.js
+++ 2-tipos-de-dados/scripts.js
@@ -8,3 +8,14 @@ console.log(10 % 2);
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
 console.log(typeof NaN);
+
+// String
+
+console.log(typeof 'Isso é uma string');
+console.log(typeof "Este texto aqui também");
+console.log(typeof `E este também`); // Template Literals
+console.log('Essa é uma string \n de duas linhas');
+console.log("Essa é uma string com 'aspas simples'");
+console.log('Essa é uma string com "aspas duplas"');
+console.log(`A soma de 2 + 2 é ${2+2}`);
+console.log("Salada" + ' de ' + `frutas`);
