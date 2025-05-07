12041f2275fff6e3e71a126fc8a36c6b38603618
Leonardo Sales <leonardosales18@gmail.com>
2025-05-07 20:54:30 -0300

    Declaracao de variaveis

--- 3-estruturas-de-programacao/scripts.js
+++ 3-estruturas-de-programacao/scripts.js
@@ -1,9 +1,18 @@
 // Declaracao de variaveis
 
-let laranjas = 5;            // Compativel ES5
-var nome = "Mateus";         // Compativel ES6
+let laranjas = 5;            // Compativel ES6
+console.log(laranjas);
+console.log(`Voce tem ${laranjas} laranjas` );
+console.log(`Voce tem ${laranjas * laranjas} laranjas` );
+laranjas = laranjas + 1;
+console.log(`Voce tem ${laranjas} laranjas` );
+
+var nome = "Mateus";         // Compativel ES5 (Nao utilizar)
 const sobrenome = "Santos";  // Compativel ES6
 // sobrenome = "Silva" (Gera erro)
 
 let varComCamelCase = "Camelo";
 let var_com_snake_case = "Cobra";
+
+//Funcoes built-in (motor da linguagem)
+
