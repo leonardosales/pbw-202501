3876071930b0ee5ec81a96fffba36563ef5e7537
Leonardo Sales <leonardosales18@gmail.com>
2025-05-07 21:58:25 -0300

    Estruturas de controle

--- 3-estruturas-de-programacao/scripts.js
+++ 3-estruturas-de-programacao/scripts.js
@@ -16,7 +16,7 @@ let varComCamelCase = "Camelo";
 let var_com_snake_case = "Cobra";
 
 //Funcoes built-in (motor da linguagem)
-console.log("Funcoes built-in (motor da linguagem)")
+console.log("\nFuncoes built-in (motor da linguagem)")
 
 let idade = prompt("Qual é a sua idade?");
 alert(`A idade digitada foi: ${idade}`);
@@ -31,3 +31,62 @@ let raizQuadrada = Math.sqrt(64);
 console.log("Raiz quadrada de 64 é: " + raizQuadrada);
 
 console.log("Numero randomico é: " + Math.random());
+
+// Estruturas de controle
+console.log("\nEstruturas de controle")
+
+if (idade > 18 && maiorNumero === 10) {
+    console.log("Pode entrar");
+}
+
+// nome = "João";
+
+if (nome === "João" && sobrenome === "Santos") {
+    console.log("Seu nome é João Santos");
+} else {
+    console.log("Seu nome não é João Santos");
+}
+
+let a = 5, b = 3;
+if (a + b === 3) {
+    console.log("O resultado é 3");
+} else if (a === 4) {
+    console.log("O valor de a é 4");
+} else if (b === 3) {
+    console.log("O valor de b é 3");
+} else {
+    console.log("Nenhuma das condições acima");
+}
+let c = a + b;
+switch(c) {
+    case c === 3:
+        console.log("O resultado é 3");
+        break;
+    case a === 4:
+        console.log("O valor de a é 4");
+        break;
+    case b === 3:
+        console.log("O valor de b é 3");
+        break;
+    default:
+        console.log("Nenhuma das condições acima");
+}
+
+let x = 10;
+while(x > 0) {
+    console.log(x);
+    x = x - 1;
+}
+
+let y = 0;
+do {
+    console.log(y);
+    y++; // y += 1;
+} while(y < 5);
+
+for (let numero = 2; numero <= 128; numero *= 2) {
+    if (numero === 64) {
+        continue;
+    }
+    console.log("O número é " + numero);
+}
