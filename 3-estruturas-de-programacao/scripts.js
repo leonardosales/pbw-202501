5396ddd3931685e71d3c710735bc4fe21d345bb1
Leonardo Sales <leonardosales18@gmail.com>
2025-05-07 21:12:29 -0300

    Funcoes built-in (motor da linguagem)

--- 3-estruturas-de-programacao/scripts.js
+++ 3-estruturas-de-programacao/scripts.js
@@ -1,4 +1,5 @@
 // Declaracao de variaveis
+console.log("Declaracao de variaveis")
 
 let laranjas = 5;            // Compativel ES6
 console.log(laranjas);
@@ -15,4 +16,18 @@ let varComCamelCase = "Camelo";
 let var_com_snake_case = "Cobra";
 
 //Funcoes built-in (motor da linguagem)
+console.log("Funcoes built-in (motor da linguagem)")
 
+let idade = prompt("Qual é a sua idade?");
+alert(`A idade digitada foi: ${idade}`);
+
+let maiorNumero = Math.max(1, 3, 5, 10);
+console.log("O maior número é: " + maiorNumero);
+
+let cubo = Math.pow(4, 3);
+console.log("4 elevado a 3 é: " + cubo);
+
+let raizQuadrada = Math.sqrt(64);
+console.log("Raiz quadrada de 64 é: " + raizQuadrada);
+
+console.log("Numero randomico é: " + Math.random());
